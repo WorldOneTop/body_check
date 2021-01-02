aba64dd5addbfe3806a2aa6f5cce827c6db3f725
WorldOneTop <dlwpdlf147@naver.com>
2021-01-02 02:52:40 +0000

    init

---
+++ Body_Check/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Body_Check project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Body_Check.settings')
+
+application = get_wsgi_application()
