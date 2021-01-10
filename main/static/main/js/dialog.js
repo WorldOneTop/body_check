1e6f54874b708e80e5446df1401a5e3b6a1d4d71
WorldOneTop <dlwpdlf147@naver.com>
2021-01-10 09:12:01 +0000

    success index.html insert to login dialog

--- main/static/main/js/dialog.js
+++ main/static/main/js/dialog.js
@@ -1,8 +1,25 @@
+// 로그인.js에서 옮김
+$(".login_button").click(function(event) {
+  event.preventDefault();
+  $(".login_form").addClass('complete');
+//     form요소 클릭시 닫는걸 막지만 버튼 클릭시 dialog창 닫기 위해
+  $("#modal-container").addClass('out');
+  $('body').removeClass('modal-active');
+});
+
+
+// form요소 클릭시 닫는거 막음(이벤트 전파 막기) bug fix
+$(".login_form").click(function(event) {
+    event.stopPropagation();
+});
+
 //다이얼로그 끄기
-$('#modal-container').click(function(){
+$('#modal-container').click(function(e){
   $(this).addClass('out');
   $('body').removeClass('modal-active');
 });
+
+
 
 //다이얼로그 띄우기
 // navpanel이 열려 있을시 클릭 안되는 버그 fix
--- main/static/main/js/login.js
+++ main/static/main/js/login.js
@@ -1,11 +1,5 @@
 
-$(".login_button").click(function(event) {
-  event.preventDefault();
-  $(".login_form").addClass('complete');
-});
-
-
-$("#test").click(function(event) {
-    alert("Asd");
-  $("form").removeClass('complete');
-});
+// $(".login_button").click(function(event) {
+//   event.preventDefault();
+//   $(".login_form").addClass('complete');
+// });
